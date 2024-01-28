5a8c2cc4f772edf7725adfcac5e4ce5fe6551372
Gurram-Madhuri <Nargarjuna.sanem@gmail.com>
2024-01-28 21:57:50 +0530

    functions

---
+++ js28.js
@@ -0,0 +1,41 @@
+/*var x=50;
+console.log(x)
+
+let y=3;
+y=5;
+console.log(y)
+
+const z=3;
+console.log(z)*/
+
+let a=40;
+let b=20;
+let c=2;
+let d="Hello";
+let e="5";
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a>b);
+console.log(a<b);
+console.log(a=b);
+console.log(a==b);
+console.log(a*c+b);
+console.log(a+b*c);
+console.log(a+b**c);
+console.log(a**c+b);
+console.log(d+"World this is "+e+"th generation");
+console.log("AND Operator",a&&b);
+console.log("type of",typeof(a));
+console.log("type of",typeof(d));
+
+
+function multi(a,b){
+
+    return a*b
+}
+
+var out=multi(4,9);
+console.log(out);
