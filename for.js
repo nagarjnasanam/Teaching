8dfbb48cc3e2f43d5ac7eac72ea7d493475f41ad
nagarjnasanam <snagarjuna2001@gmail.com>
2024-02-07 22:45:29 +0530

    teached for loop in javascript

---
+++ for.js
@@ -0,0 +1,29 @@
+
+
+
+
+const numbers = [4,2,5,7,2,9,6,8,3,7,5]
+                // 0,1,2 ......... lemgt -1
+
+                // console.log(numbers[5])
+
+                // for(let i=0;i<numbers.length;i++){
+                //     console.log("i",i,numbers[i])
+                // }
+
+const data = { a:10000000, b:2,c:6786,g:909}
+
+console.log("a with []",data['a'])
+console.log("a with .",data.a)
+
+// const keys = ["a","b","c","g"]
+const keys = Object.keys(data)
+for(let i =0 ;i<keys.length;i++){
+    console.log(keys[i])
+    console.log(data[keys[i]])
+
+}
+
+
+
+
---
+++ forin.js
@@ -0,0 +1,40 @@
+//  for ... in
+const numbers = [4,6,2,8,1]
+// for in
+for(num in numbers){
+    console.log("num",num,numbers[num])
+}
+
+
+// for ..... of
+for(num of numbers){
+    console.log("by using of",num)
+}
+
+
+
+// usin=g default for loop
+for(num =0 ;num<numbers.length;num++){
+    console.log("numjhgggg",num)
+}
+
+
+
+// objects
+const dataObject = { a:10000000, b:2,c:6786,g:909}
+const cars = [{ name:"audi", color:"red"},{ name:"benz", color:"blue"},{ name:"lamborgini", color:"orange"}]
+
+
+//for in
+
+for(data in dataObject){
+    console.log("uding for in",data,dataObject[data])
+}
+
+// for of
+for(data of cars){
+    console.log("for of",data)
+    for(key in data){
+        console.log("uding for in",key,data[key])
+    }
+}
